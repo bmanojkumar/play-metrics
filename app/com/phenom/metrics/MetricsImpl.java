8d20ce4b2ae851970f7fbfb049c44fa0f8c5bc6d
Manoj <manoj.boddupalli@phenompeople.com>
2016-12-10 11:44:47 +0530

    Graphite Reporter and sbt assembly plugin added

--- app/com/phenom/metrics/MetricsImpl.java
+++ app/com/phenom/metrics/MetricsImpl.java
@@ -11,41 +11,55 @@ import play.Configuration;
 import play.Logger;
 import play.inject.ApplicationLifecycle;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetSocketAddress;
+import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class MetricsImpl implements Metrics {
 
-    private Logger.ALogger logger = Log.Logger;
+    private static Logger.ALogger logger = Log.Logger;
 
     @Inject
     MetricsImpl(ApplicationLifecycle lifecycle, Configuration configuration) {
         logger.debug("Metrics Plugin Initializing");
+        String HOST_NAME = execCommandAndReadToString("hostname");
+        if(HOST_NAME.length() == 0) HOST_NAME = "HOST_NAME_NOT_FOUND";
+        HOST_NAME = HOST_NAME.replace(".", "_");
         final String GRAPHITE_HOST = configuration.getString("metrics.graphiteHost");
         final int GRAPHITE_PORT = configuration.getInt("metrics.graphitePort");
         final String SERVICE_NAME = configuration.getString("metrics.serviceName");
         final int PING_INTERVAL = configuration.getInt("metrics.pingInterval");
         final MetricRegistry registry = SharedMetricRegistries.getOrCreate("phenommetrics");
-        /*final Graphite graphite = new Graphite(new InetSocketAddress(GRAPHITE_HOST, GRAPHITE_PORT));
+        final Graphite graphite = new Graphite(new InetSocketAddress(GRAPHITE_HOST, GRAPHITE_PORT));
         final GraphiteReporter graphiteReporter = GraphiteReporter.forRegistry(registry)
-                .prefixedWith(SERVICE_NAME+"."+"HOST_NAME"+".")
+                .prefixedWith(SERVICE_NAME+"."+HOST_NAME+".")
                 .convertRatesTo(TimeUnit.SECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
                 .filter(MetricFilter.ALL)
                 .build(graphite);
-        graphiteReporter.start(PING_INTERVAL, TimeUnit.SECONDS);*/
-        //
-        ConsoleReporter reporter = ConsoleReporter.forRegistry(registry)
-                .convertRatesTo(TimeUnit.SECONDS)
-                .convertDurationsTo(TimeUnit.MILLISECONDS)
-                .build();
-        reporter.start(1, TimeUnit.SECONDS);
-        //
+        graphiteReporter.start(PING_INTERVAL, TimeUnit.SECONDS);
         lifecycle.addStopHook(()-> {
             logger.debug("Shutting down metrics plugin");
-            //graphiteReporter.stop();
+            graphiteReporter.stop();
             return CompletableFuture.completedFuture(null);
         });
+    }
+
+    public static String execCommandAndReadToString(String execCommand) {
+        try {
+            Process proc = Runtime.getRuntime().exec(execCommand);
+            try (InputStream stream = proc.getInputStream()) {
+                try (Scanner s = new Scanner(stream).useDelimiter("\\A")) {
+                    return s.hasNext() ? s.next() : "";
+                }
+            }
+        }
+        catch (Exception ex) {
+            logger.error("Error fetching hostname");
+            return "";
+        }
     }
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -15,6 +15,8 @@ addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.6")
 // plugin if you prefer not to have this feature, or disable on a per project
 // basis using disablePlugins(PlayEnhancer) in your build.sbt
 addSbtPlugin("com.typesafe.sbt" % "sbt-play-enhancer" % "1.1.0")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.3")
+
 
 // Play Ebean support, to enable, uncomment this line, and enable in your build.sbt using
 // enablePlugins(PlayEbean).
